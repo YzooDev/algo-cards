183c8394b5de8481a56075a867e3d7945294192e
YzooDev <remi.bezes.dev@gmail.com>
2026-03-13 13:47:42 +0100

    feat : add function profil + fix all functions afficher

--- index.php
+++ index.php
@@ -18,48 +18,74 @@ array_push($tabData, $USERS_HUMAN, $USERS_PET, $USERS_XENO);
 
 //------------------------------------------------------
 // 6) Créer la fonction afficherHumain() qui prendra un tableau associatif en paramètre
-function afficherHumain($tableau) {
-    for ($i=0 ; $i < sizeof($tableau); $i++) { 
+function afficherHumain($array) { 
         echo "<article style= 'border-bottom : 3px solid black '>
-            <h2>nom : ".$tableau[$i]["name"]."</h2>
-            <p>email : ".$tableau[$i]["email"]."</p>
-            <p>age : ".$tableau[$i]["age"]." ans</p>
+            <h2>nom : ".$array["name"]."</h2>
+            <p>email : ".$array["email"]."</p>
+            <p>age : ".$array["age"]." ans</p>
         </article>";
-    };
 };
 
 //------------------------------------------------------
 // 8) Créer la fonction afficherAnimal() qui prendra un tableau associatif en paramètre.
 
-function afficherAnimal($tableau) {
-    for ($i=0 ; $i < sizeof($tableau); $i++) { 
+function afficherAnimal($array) {
         echo "<article style= 'border-bottom : 3px solid black '>
-            <h2>nom : ".$tableau[$i]["name"]."</h2>
-            <p>espece : ".$tableau[$i]["espece"]."</p>
-            <p>age : ".$tableau[$i]["age"]." ans</p>
-            <p>propriétaire : ".$tableau[$i]["propriétaire"]."</p>
+            <h2>nom : ".$array["name"]."</h2>
+            <p>espece : ".$array["espece"]."</p>
+            <p>age : ".$array["age"]." ans</p>
+            <p>propriétaire : ".$array["propriétaire"]."</p>
         </article>";
-    };
 };
 
 //------------------------------------------------------
 // 10) Créer la fonction afficherXeno() qui prendra un tableau associatif en paramètre.
 
-function afficherXeno($tableau) {
-    for ($i=0 ; $i < sizeof($tableau); $i++) { 
+function afficherXeno($array) {
         echo "<article style= 'border-bottom : 3px solid black '>
-            <h2>nom : ".$tableau[$i]["name"]."</h2>
-            <p>espece : ".$tableau[$i]["espece"]."</p>
-            <p>age : ".$tableau[$i]["age"]." ans</p>
-            <p>niveau de menace: ".$tableau[$i]["menace"]."</p>
+            <h2>nom : ".$array["name"]."</h2>
+            <p>espece : ".$array["espece"]."</p>
+            <p>age : ".$array["age"]." ans</p>
+            <p>niveau de menace: ".$array["menace"]."</p>
         </article>";
-    };
 };
 
+//------------------------------------------------------
+// 12) Créer une fonction profil() qui prend en paramètre un tableau.
+
+function profil($array) {
+    // 13) Faire en sorte que la fonction profil() parcourt chaque tableau associatif du tableau en paramètre.
+    $display = 0;
+    for ($i=0; $i < sizeof($array); $i++) { 
+        // AVEC IF
+        // if ($array[$i]["type"] == "humain") {
+        //     $display = afficherHumain($array[$i]);
+        // } else if ("type" == "animal de compagnie"){
+        //     $display = afficherAnimal($array[$i]);
+        // } else if ("type" == "Xeno") {
+        //     $display = afficherXeno($array[$i]);
+        // }
+
+        // AVEC SWITCH
+        switch($array[$i]["type"]){
+        case "humain":
+            $display = afficherHumain($array[$i]);
+            break;
+        case "animal de compagnie":
+            $display = afficherAnimal($array[$i]);
+            break;
+        case "Xeno":
+            $display = afficherXeno($array[$i]);
+            break;
+        }
+    }
+    return $display;
+}
 ?>
     <main>
-        <?php afficherHumain($USERS_HUMAN) ?>
-        <?php afficherAnimal($USERS_PET) ?>
-        <?php afficherXeno($USERS_XENO) ?>
+        <?php afficherHumain($USERS_HUMAN[0]) ?>
+        <?php afficherAnimal($USERS_PET[0]) ?>
+        <?php afficherXeno($USERS_XENO[0]) ?>
+        <?php profil($USERS_HUMAN) ?>
     </main>
 <?php include_once "./header.php" ;?>
